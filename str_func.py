c710d524bea5bb557b53710a9abe8d279a1df88a
Sveta Lalayan <lalayan-svetlana@list.ru>
2024-03-06 20:56:40 +0300

    new commit

--- str_func.py
+++ str_func.py
@@ -1,5 +1,13 @@
 def capitalize():
     input()
 '''функция выводит все с большой буквы'''
+'''выводит все с большой буквы'''
 result = input()
 print(result.upper())
+
+
+def capitalize_words():
+    input()
+'''делает первые буквы слов заглавными'''
+result = input()
+print(result.title())
